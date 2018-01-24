51a6a1d06e3e7ed33e3ef5487f8c76454c2eac84
Matrixeigs <matrixeigs@gmail.com>
2018-01-24 09:13:13 +0800

    Update start-up information modelling method

--- start_up/start_up_lems.py
+++ start_up/start_up_lems.py
@@ -34,6 +34,44 @@ def start_up():
     microgrid["Load_dc"]["FLEX"] = default_command["CRITICAL"]
     microgrid["Load_ndc"]["FLEX"] = default_command["NON_CRITICAL"]
 
+    T_short = default_look_ahead_time_step[
+        "Look_ahead_time_opf_time_step"]  # The look ahead time step for short term operation
+    T_middle = default_look_ahead_time_step[
+        "Look_ahead_time_ed_time_step"]  # The look ahead time step for middle term operation
+    T_long = default_look_ahead_time_step[
+        "Look_ahead_time_uc_time_step"]  # The look ahead time step for long term operation
+    # Update information
+    local_mg_short = deepcopy(microgrid)
+    local_mg_middle = deepcopy(microgrid)
+    local_mg_long = deepcopy(microgrid)
+
+    # Generate middle term operation model for local ems, these information should be updated according to the database of resource manager
+    local_mg_middle["UG"]["GEN_STATUS"] = [local_mg_middle["UG"]["GEN_STATUS"]] * T_middle
+    local_mg_middle["DG"]["GEN_STATUS"] = [local_mg_middle["DG"]["GEN_STATUS"]] * T_middle
+    local_mg_middle["PV"]["N"] = [local_mg_middle["PV"]["N"]] * T_middle
+    local_mg_middle["PV"]["PMAX"] = [local_mg_middle["PV"]["PMAX"]] * T_middle
+    local_mg_middle["WP"]["N"] = [local_mg_middle["WP"]["N"]] * T_middle
+    local_mg_middle["WP"]["PMAX"] = [local_mg_middle["WP"]["PMAX"]] * T_middle
+    local_mg_middle["Load_ac"]["STATUS"] = [local_mg_middle["Load_ac"]["STATUS"]] * T_middle
+    local_mg_middle["Load_nac"]["STATUS"] = [local_mg_middle["Load_nac"]["STATUS"]] * T_middle
+    local_mg_middle["Load_dc"]["STATUS"] = [local_mg_middle["Load_dc"]["STATUS"]] * T_middle
+    local_mg_middle["Load_ndc"]["STATUS"] = [local_mg_middle["Load_ndc"]["STATUS"]] * T_middle
+    local_mg_middle["BIC"]["STATUS"] = [local_mg_middle["BIC"]["STATUS"]] * T_middle
+    local_mg_middle["ESS"]["STATUS"] = [local_mg_middle["ESS"]["STATUS"]] * T_middle
+
+    # Generate long term operation model for local ems
+    local_mg_long["UG"]["GEN_STATUS"] = [local_mg_long["UG"]["GEN_STATUS"]] * T_long
+    local_mg_long["DG"]["GEN_STATUS"] = [local_mg_long["DG"]["GEN_STATUS"]] * T_long
+    local_mg_long["PV"]["N"] = [local_mg_long["PV"]["N"]] * T_long
+    local_mg_long["PV"]["PMAX"] = [local_mg_long["PV"]["PMAX"]] * T_long
+    local_mg_long["WP"]["N"] = [local_mg_long["WP"]["N"]] * T_long
+    local_mg_long["WP"]["PMAX"] = [local_mg_long["WP"]["PMAX"]] * T_long
+    local_mg_long["Load_ac"]["STATUS"] = [local_mg_long["Load_ac"]["STATUS"]] * T_long
+    local_mg_long["Load_nac"]["STATUS"] = [local_mg_long["Load_nac"]["STATUS"]] * T_long
+    local_mg_long["Load_dc"]["STATUS"] = [local_mg_long["Load_dc"]["STATUS"]] * T_long
+    local_mg_long["Load_ndc"]["STATUS"] = [local_mg_long["Load_ndc"]["STATUS"]] * T_long
+    local_mg_long["BIC"]["STATUS"] = [local_mg_long["BIC"]["STATUS"]] * T_long
+    local_mg_long["ESS"]["STATUS"] = [local_mg_long["ESS"]["STATUS"]] * T_long
 
     return microgrid
 
--- start_up/start_up_uems.py
+++ start_up/start_up_uems.py
@@ -1,14 +1,30 @@
 from copy import deepcopy
 from modelling.devices import transmission_lines
+from configuration.configuration_time_line import default_look_ahead_time_step
 
-def start_up(microgrid):
+def start_up(microgrid,microgrid_middle,microgrid_long):
     """
     Start up of universal energy management system, which is depended on the start up of local energy management system.
-    :param local ems model :  (short)
-    :return: universal ems model (short)
+    :param microgrid: short-term information model
+    :param microgrid_middle: middle-term information model
+    :param microgrid_long: long-term information model
+    :return:
     """
     microgrid = deepcopy(microgrid)
 
     microgrid["LINE"] = deepcopy(transmission_lines.Line)
 
-    return microgrid
+    T_middle = default_look_ahead_time_step[
+        "Look_ahead_time_ed_time_step"]  # The look ahead time step for middle term operation
+    T_long = default_look_ahead_time_step[
+        "Look_ahead_time_uc_time_step"]  # The look ahead time step for long term operation
+
+    microgrid_middle = deepcopy(microgrid_middle)
+    microgrid_middle["LINE"] = deepcopy(transmission_lines.Line)
+    microgrid_middle["LINE"] = [microgrid_middle["LINE"]["STATUS"]] * T_middle
+
+    microgrid_long = deepcopy(microgrid_long)
+    microgrid_long["LINE"] = deepcopy(transmission_lines.Line)
+    microgrid_long["LINE"] = [microgrid_long["LINE"]["STATUS"]] * T_long
+
+    return microgrid,microgrid_middle,microgrid_long
